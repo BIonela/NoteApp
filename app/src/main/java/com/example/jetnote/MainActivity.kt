c60b0abba1042a0ef6e3f95b687d498ffdb78413
DESKTOP-14NFG4G\ionela <ionelamihaela003@gmail.com>
2023-08-24 16:33:55 +0300

    first commit

---
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.jetnote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.jetnote.screen.NoteScreen
+import com.example.jetnote.screen.NoteViewModel
+import com.example.jetnote.ui.theme.JetNoteTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetNoteTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val noteViewModel = viewModel<NoteViewModel>()
+                    NotesApp(noteViewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel) {
+    val notesList = noteViewModel.noteList.collectAsState().value
+
+    NoteScreen(notes = notesList,
+        onAddNote = {
+            noteViewModel.addNote(it)
+        },
+        onRemoveNote = {
+            noteViewModel.removeNote(it)
+        })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetNoteTheme {
+
+    }
+}
